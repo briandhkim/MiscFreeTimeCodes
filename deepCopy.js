e7622823f26dd0da5ba8517ee1572c30955d62c0
briandhkim <w1057216@gmail.com>
2018-03-16 00:43:13 -0700

    obj deepcopy

---
+++ deepCopy.js
@@ -0,0 +1,51 @@
+/**
+In javascript, objects and arrays are reference variables;
+therefore, if a new variable is assigned an existing object,
+the new variable and existing variable would point (reference)
+to the same object
+the purpose of this function is to create a copy of the object 
+or array with new reference point
+**/
+
+
+const object = {
+    x: '1',
+    y: {
+        a:'2',
+        b:'3'
+    }
+}
+
+let deepCopy =(obj)=>{
+
+    for(var idx in obj){
+        if(typeof obj[idx] =='object'){
+            return deepCopy(obj[idx]);
+        }else{
+      
+            if(Array.isArray(obj)){
+                const returnArr= [];
+                for(var i in obj){
+                    returnArr.push(obj[i]);
+                }
+                return returnArr;
+            }
+      
+            let returnObj = {};
+            for(var idx in obj){
+                returnObj[idx] = obj[idx];
+            }
+            return returnObj;
+      
+        }   
+    }
+}
+
+let newO = deepCopy(object);
+// console.log(newO, object);
+console.log("object check", newO === object);
+
+const arr1= [1,2,3];
+const newArr = deepCopy(arr1);
+console.log(arr1, newArr);
+console.log(arr1===newArr);
